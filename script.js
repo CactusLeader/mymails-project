47737d460d148fa44d94715bb6edf45298cbcf1b
Maxime Gomez-Duret <maxime.gomezduret@gmail.com>
2022-01-07 15:49:25 +0100

    champ de recherche

---
+++ script.js
@@ -0,0 +1,47 @@
+let messageCount = $(".trash").length;
+$("#count").text(messageCount);
+
+$("body").on(
+    "click", 
+    '.trash',
+    function() {
+        $(this).parent().remove();
+        let messageCount = $(".trash").length;
+        $("#count").text(messageCount);
+    }
+)
+
+$("#btn-add").click(
+    function(){
+        const messageInput = $("#add-message").val();
+        $('body').append(
+        `<div class="row last-row">
+            <img class="avatar" src="avatar-4.jpg">
+            <div>
+                <h6>Eric Dupont</h6>
+                <p>${messageInput}</p>
+            </div>
+            <img class="trash" src="trash.png">
+        </div>`
+        )
+        $("#add-message").val("");
+        let messageCount = $(".trash").length;
+        $("#count").text(messageCount);
+    }
+)
+
+$("#btn-search").click(
+    function(){
+        let searchInput = $("#search-message").val();
+        $("h6").each(
+            function(){
+                if (searchInput != $(this).text()) {
+                    $(this).parent().parent().fadeOut(600);
+                } else {
+                    $(this).parent().parent().fadeIn(600);
+                }
+            }
+        )
+        $("#search-message").val("");
+    }
+)
